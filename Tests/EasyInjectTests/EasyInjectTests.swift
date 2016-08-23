8f80f659c2cb8b845364b7d552a86a52a28eef80
Valentin Knabel <develop@vknabel.com>
2016-08-23 10:14:39 +0200

    Tries to fix linux tests

--- Tests/EasyInjectTests/EasyInjectTests.swift
+++ Tests/EasyInjectTests/EasyInjectTests.swift
@@ -9,7 +9,7 @@
 import XCTest
 
 #if os(Linux)
-public func allTests() -> [XCTestCaseEntry] {
+public func allTests() -> [(String, (XCTestCase) -> () -> Void)] {
     return [
         testCase(StrictInjectorTests.allTests),
         testCase(LazyInjectorTests.allTests),
